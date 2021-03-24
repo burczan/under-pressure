c93ea51f5e8fd0ed53978214960264ed7c0f90e8
burczan <natalia.wajnor@gmail.com>
2021-03-24 08:42:09 +0100

    add Message component

---
+++ client/src/components/Message/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import cx from 'classnames';
+import { Color } from '../types';
+
+type MessageProps = {
+  children: React.ReactNode;
+  header?: React.ReactNode;
+  color?: Color;
+  messageClassName?: string;
+};
+
+export const Message = ({ children, header, color, messageClassName }: MessageProps) => {
+  return (
+    <article className={cx('message', color, messageClassName)}>
+      {header && (
+        <div className="message-header">
+          {header}
+        </div>
+      )}
+      <div className="message-body">
+        {children}
+      </div>
+    </article>
+  );
+};
