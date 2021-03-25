2e16cabecfc654193403827b413455c54874b44c
burczan <natalia.wajnor@gmail.com>
2021-03-25 09:53:07 +0100

    add PressureChange component

--- packages/client/src/components/PressureDetails/index.tsx
+++ packages/client/src/components/PressureDetails/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Message } from '../../common/components/Message';
+import { PressureChange } from '../PressureChange';
 
 export type WeatherHistory = {
   date: string | undefined,
@@ -22,11 +23,16 @@ type PressureDetailsProps = {
 };
 
 export const PressureDetails = ({ details }: PressureDetailsProps) => {
+  const firstValue = details.pressure[0].value;
+  const lastValue = details.pressure[details.pressure.length - 1].value;
+  const pressureChangeIn4Hours = firstValue - lastValue;
+
   return (
     <Message
       header={`${details.date}, ${details.location.name} (${details.location.altitude})`}
       messageClassName="is-size-5"
     >
+      <PressureChange value={pressureChangeIn4Hours}/>
       {details.pressure.map(({ hour, value, unit }) => {
         return (
           <p key={hour}>
