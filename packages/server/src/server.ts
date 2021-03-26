76139ada736bcfaae74950f47c5b8e2188ecd73d
burczan <natalia.wajnor@gmail.com>
2021-03-26 15:05:01 +0100

    add eslint to server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express';
 import cors from 'cors';
 import { getWeatherHistory } from './utility/weatherHistory';
@@ -9,8 +10,8 @@ app.use(cors());
 app.get('/weather_history', async (_req, res) => {
   const data = await getWeatherHistory();
   res.send(data);
-})
+});
 
 app.listen(4000, () => {
-  console.log(`Running on port 4000.`);
+  console.log('Running on port 4000.');
 });
